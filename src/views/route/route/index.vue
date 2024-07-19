c59740a952a6ca794b73f9bbedd849792c974f8a
联想 <1335245845@qq.com>
2024-07-19 20:08:38 +0800

    资源调度一张图-路径规划前端弹窗修改

--- src/views/route/route/index.vue
+++ src/views/route/route/index.vue
@@ -6,69 +6,122 @@
       <el-button class="el-button--primary" @click="removeAll">清空所有实体</el-button>
       <el-button class="el-button--primary" @click="removePoint">删除障碍区域</el-button>
       <el-button class="el-button--primary" @click="removePolyline">删除路径规划</el-button>
+      <el-button class="el-button--primary" @click="emergencyResourceInformation">应急资源信息</el-button>
     </el-form>
+    <RouterPanel :visible="popupVisible" :position="popupPosition" :popupData="popupData" @wsSendPoint="wsSendPoint"/>
   </div>
 </template>
 
 <script>
+
 import * as Cesium from 'cesium'
-// import 'cesium/Source/Widgets/widgets.css'
+import 'cesium/Source/Widgets/widgets.css'
 import CesiumNavigation from "cesium-navigation-es6";
 import {initCesium} from '@/cesium/tool/initCesium.js'
-// import geojsonmap from '@/assets/geoJson/map.json'
-// import geojsonmap from '@/assets/geoJson/YaanRoadGeoJson.json'
 import axios from "axios";
 import start from '@/assets/start.svg'
 import end from '@/assets/end.svg'
 import {Entity} from "cesium";
+import RouterPanel from '@/components/Cesium/RouterPanel.vue'
+import cesiumPlot from '@/cesium/plot/cesiumPlot.js'
+import { getPloy } from '@/api/system/plot'
+import { initWebSocket } from '@/cesium/WS.js'
+
 
 export default {
+  components: {
+    RouterPanel,
+  },
   name: "index",
-  data(){
-    return{
-      pos:[],
-      areas:[]
+  data() {
+    return {
+      pos: [],
+      areas: [],
+      //-----------弹窗部分-------------------
+      selectedEntityHighDiy: null,
+      popupPosition: {x: 0, y: 0}, // 弹窗显示位置，传值给子组件
+      popupVisible: false, // 弹窗的显示与隐藏，传值给子组件
+      popupData: {}, // 弹窗内容，传值给子组件
     }
   },
   mounted() {
-    this.init()
+    this.init();
+    this.entitiesClickPonpHandler()
+    this.watchTerrainProviderChanged();
+    cesiumPlot.init(window.viewer, this.websock, this.$store)
+    console.log(" this.$router.currentRoute11111111:", this.$router.currentRoute)
+    // this.id = this.$router.currentRoute.query.id
+    this.id = 'ckwtest123'
+    this.initPlot(this.id)
+    this.initWebsocket()
+    //---------------------------
   },
-  methods:{
+  methods: {
     init() {
-      let viewer = initCesium(Cesium)
-      viewer._cesiumWidget._creditContainer.style.display = 'none' // 隐藏版权信息
-      window.viewer = viewer
-      let options = {}
-      // 用于在使用重置导航重置地图视图时设置默认视图控制。接受的值是Cesium.Cartographic 和 Cesium.Rectangle.
-      options.defaultResetView = Cesium.Cartographic.fromDegrees(103.00, 29.98, 1500, new Cesium.Cartographic)
-      // 用于启用或禁用罗盘。true是启用罗盘，false是禁用罗盘。默认值为true。如果将选项设置为false，则罗盘将不会添加到地图中。
-      options.enableCompass = true
-      // 用于启用或禁用缩放控件。true是启用，false是禁用。默认值为true。如果将选项设置为false，则缩放控件将不会添加到地图中。
-      options.enableZoomControls = true
-      // 用于启用或禁用距离图例。true是启用，false是禁用。默认值为true。如果将选项设置为false，距离图例将不会添加到地图中。
-      options.enableDistanceLegend = true
-      // 用于启用或禁用指南针外环。true是启用，false是禁用。默认值为true。如果将选项设置为false，则该环将可见但无效。
-      options.enableCompassOuterRing = true
+      console.log(" this.$router.currentRoute:2222222", this.$router.currentRoute)
+      let viewer = initCesium(Cesium);
+      viewer._cesiumWidget._creditContainer.style.display = 'none'; // 隐藏版权信息
+      window.viewer = viewer;
+      let options = {};
+      options.defaultResetView = Cesium.Cartographic.fromDegrees(103.00, 29.98, 1500, new Cesium.Cartographic);
+      options.enableCompass = true;
+      options.enableZoomControls = true;
+      options.enableDistanceLegend = true;
+      options.enableCompassOuterRing = true;
       options.resetTooltip = "重置视图";
       options.zoomInTooltip = "放大";
       options.zoomOutTooltip = "缩小";
-      //新版必须new  CesiumNavigation ,可以查看作者github
-      window.navigation = new CesiumNavigation(viewer, options)
-      document.getElementsByClassName('cesium-geocoder-input')[0].placeholder = '请输入地名进行搜索'
-      document.getElementsByClassName('cesium-baseLayerPicker-sectionTitle')[0].innerHTML = '影像服务'
-      document.getElementsByClassName('cesium-baseLayerPicker-sectionTitle')[1].innerHTML = '地形服务'
-
-      // 添加路网
-      // viewer.dataSources.add(
-      //   Cesium.GeoJsonDataSource.load(
-      //     geojsonmap,
-      //     {
-      //       stroke: Cesium.Color.RED,   // 边框颜色
-      //       // fill: Cesium.Color.RED.withAlpha(0.5),  // 填充颜色
-      //       strokeWidth: 3, // 边框宽度
-      //     })
-      // );
-
+      window.navigation = new CesiumNavigation(viewer, options);
+      document.getElementsByClassName('cesium-geocoder-input')[0].placeholder = '请输入地名进行搜索';
+      document.getElementsByClassName('cesium-baseLayerPicker-sectionTitle')[0].innerHTML = '影像服务';
+      document.getElementsByClassName('cesium-baseLayerPicker-sectionTitle')[1].innerHTML = '地形服务';
+    },
+    initPlot(){
+      getPloy({id:"ckwtest123"}).then(res=>{
+        this.plotData = res
+        let data = res
+        let pointArr = data.filter( e => e.drawtype === 'point')
+        let polylineArr = data.filter( e => e.drawtype === 'polyline')
+        let polygonArr = data.filter( e => e.drawtype === 'polygon')
+        //画点
+        this.drawPoint(pointArr)
+        this.drawPolyline(polylineArr)
+        this.drawPolygon(polygonArr)
+      })
+    },
+    drawPoint(pointArr){
+      pointArr.forEach(element=>{
+        window.viewer.entities.add({
+          id:element.drawid,
+          position: Cesium.Cartesian3.fromDegrees(Number(element.longitude) , Number(element.latitude), Number(element.height)),
+          billboard: {
+            image: element.img,
+            eyeOffset: new Cesium.Cartesian3(0, 0, 0),//与坐标位置的偏移距离
+            color: Cesium.Color.WHITE.withAlpha(1),//颜色
+            scale: 0.8,//缩放比例
+            heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,// 绑定到地形高度,让billboard贴地
+            depthTest: false,//禁止深度测试但是没有下面那句有用
+            disableDepthTestDistance: Number.POSITIVE_INFINITY//不再进行深度测试（真神）
+          },
+          properties:{
+            type:element.pointtype,
+            time:element.timestamp,
+            name:element.pointname,
+            lon:element.longitude,
+            lat:element.latitude,
+            describe:element.pointdescribe,
+            id:element.drawid
+          }
+        })
+      })
+    },
+    initWebsocket() {
+      this.websock = initWebSocket()
+      this.websock.id = this.id
+    },
+    // ws发送数据（只有点的是在这里）
+    wsSendPoint(data) {
+      this.websock.send(data)
     },
     /** 计算两个坐标的距离，单位米 **/
     Distance(lng1, lat1, lng2, lat2) {
@@ -121,13 +174,13 @@ export default {
           propertiesId.push(billBoardId)
         }
         if(that.pos.length===2){
-          axios.post('http://127.0.0.1:8089/map_test/way', { // 'http://127.0.0.1:8089/map_test/way'
+          axios.post('http://192.168.0.107:8089/map_test/way', { // 'http://127.0.0.1:8089/map_test/way'
             pathWay:that.pos,
             hardAreas:that.areas
           }).then(function (response) {
             console.log(response.data,123)
-              that.polylineD(response.data.path,propertiesId)
-              that.pos = []
+            that.polylineD(response.data.path,propertiesId)
+            that.pos = []
           }).catch(err=>{
             alert("无可行路径")
           })
@@ -255,7 +308,145 @@ export default {
           handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
         }
       }, Cesium.ScreenSpaceEventType.LEFT_CLICK)
-    }
+    },
+    emergencyResourceInformation(){
+      // 调用 initPlot 方法
+      this.initPlot();
+    },
+    isTerrainLoaded() {
+      let terrainProvider = window.viewer.terrainProvider;
+      if (terrainProvider instanceof Cesium.EllipsoidTerrainProvider) {
+        // console.log("地形未加载")
+        return false;
+      } else if (Cesium.defined(terrainProvider)) {
+        // 如果terrainProvider已定义，但不是EllipsoidTerrainProvider，
+        // 则表示已经设置了其他地形提供者
+        // console.log("地形已加载")
+        return true;
+      }
+      // console.log("地形未加载")
+      return false;
+    },
+    watchTerrainProviderChanged() {
+      let that = this
+      window.viewer.scene.terrainProviderChanged.addEventListener(terrainProvider => {
+        this.popupVisible = false // 地形改变时关闭弹窗
+        let tzs = []
+        if(that.modelName === 1){
+          tzs[0] = 9
+          tzs[1] = -567
+        }else {
+          tzs[0] = 15
+          tzs[1] = -557
+        }
+        if(that.isTerrainLoaded()){
+          that.changeHeight(tzs[0])
+          that.tz = tzs[0]
+          that.find()
+        }else {
+          that.changeHeight(tzs[1])
+          that.tz = tzs[1]
+          that.find()
+        }
+      });
+    },
+    // 所有entity实体类型点击事件的handler（billboard、polyline、polygon）
+    entitiesClickPonpHandler() {
+      let that = this
+      window.viewer.screenSpaceEventHandler.setInputAction(async (click) => {
+        // 1-1 获取点击点的信息（包括）
+        let pickedEntity = window.viewer.scene.pick(click.position);
+        window.selectedEntity = pickedEntity?.id
+        // 2-1 判断点击物体是否为点实体（billboard）
+        if (Cesium.defined(pickedEntity) && window.selectedEntity !== undefined && window.selectedEntity._billboard !== undefined) {
+          // const cesiumPosition = that.selectedEntity.position.getValue(
+          //     window.viewer.clock.currentTime
+          // );
+          //
+          // 2-2 获取点击点的经纬度
+          let ray = viewer.camera.getPickRay(click.position)
+          let position = viewer.scene.globe.pick(ray, viewer.scene)
+          // 2-3 将笛卡尔坐标转换为地理坐标角度,再将地理坐标角度换为弧度
+          let cartographic = Cesium.Cartographic.fromCartesian(position);
+          let latitude = Cesium.Math.toDegrees(cartographic.latitude);
+          let longitude = Cesium.Math.toDegrees(cartographic.longitude);
+
+          // 2-4-1 将经纬度和高度生成新的笛卡尔坐标，用来解决弹窗偏移（不加载地形的情况）
+          let height = 0
+          that.selectedEntityHighDiy = Cesium.Cartesian3.fromDegrees(longitude, latitude, height);// 这种可以存data吗？？？？？？？？？？？？？？？
+          // 2-4-2 加载地形时，构建虚拟的已添加实体，让弹窗定位到虚拟的实体上
+          if (this.isTerrainLoaded()) {
+            const cesiumPosition = window.selectedEntity.position.getValue(window.viewer.clock.currentTime);//获取时间？？？？？？？？？？？？
+            let l = Cesium.Cartographic.fromCartesian(position)
+            let lon = Cesium.Math.toDegrees(l.longitude)
+            let lat = Cesium.Math.toDegrees(l.latitude)
+            let hei = l.height
+            let height
+            // 将笛卡尔坐标转换为地理坐标角度
+            let cartographic = Cesium.Cartographic.fromCartesian(position);
+            // 将地理坐标角度换为弧度
+            let latitude = Cesium.Math.toDegrees(cartographic.latitude);
+            let longitude = Cesium.Math.toDegrees(cartographic.longitude);
+            height = window.viewer.scene.globe.getHeight(cartographic) // 获取当前位置的高度
+            // 将经纬度和高度生成新的笛卡尔坐标
+            that.selectedEntityHighDiy = Cesium.Cartesian3.fromDegrees(Number(longitude.toFixed(6)), Number(latitude.toFixed(6)), height);
+            // console.log("虚拟位置",{longitude, latitude, height},"真实位置",{lon,lat,hei})
+          }
+          // 2-5 更新弹窗位置
+          // that.selectedEntity = window.selectedEntity
+          that.popupData = {
+            type: window.selectedEntity.properties.type.getValue(),
+            time: window.selectedEntity.properties.time.getValue(),
+            name: window.selectedEntity.properties.name.getValue(),
+            lon: window.selectedEntity.properties.lon.getValue(),
+            lat: window.selectedEntity.properties.lat.getValue(),
+            describe: window.selectedEntity.properties.describe.getValue(),
+          };
+          this.popupVisible = true; // 显示弹窗
+          this.updatePopupPosition(); // 更新弹窗的位置
+
+        } else {
+          this.popupVisible = false; // 隐藏弹窗
+        }
+        // 3-1 选中面时触发
+        if (Cesium.defined(pickedEntity) && window.selectedEntity._polygon !== undefined) {
+          that.showPolygon = true
+          // that.polygonPosition = window.selectedEntity
+        } else {
+          this.showPolygon = false
+        }
+        // 4-1选中线时触发
+        if (Cesium.defined(pickedEntity) && window.selectedEntity._polyline !== undefined) {
+          let status = cesiumPlot.drawPolylineStatus()
+          if (status === 0) {
+            that.showPolyline = true
+            // that.polylinePosition = window.selectedEntity
+          }
+        } else {
+          this.showPolyline = false
+        }
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK); //LEFT_DOUBLE_CLICK
+
+      // 必须有这个，拖动地图弹窗位置才会跟着移动
+      window.viewer.screenSpaceEventHandler.setInputAction(movement => {
+        if (that.popupVisible && window.selectedEntity) {
+          that.updatePopupPosition(); // 更新弹窗的位置
+        }
+      }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+    },
+    //--------------------弹窗----------------------
+    // 判断是否有高程
+    // 更新弹窗的位置
+    updatePopupPosition() {
+      // 笛卡尔3转笛卡尔2（屏幕坐标）
+      const canvasPosition = Cesium.SceneTransforms.wgs84ToWindowCoordinates(window.viewer.scene, this.selectedEntityHighDiy)
+      if (canvasPosition) {
+        this.popupPosition = {
+          x: canvasPosition.x,//+ 20,
+          y: canvasPosition.y //- 60 // 假设弹窗应该在图标上方 50px 的位置
+        };
+      }
+    },
   }
 }
 </script>
@@ -268,11 +459,9 @@ export default {
   position: absolute;
   padding: 15px;
   border-radius: 5px;
-  /*width: 500px;*/
-  /*height: 200px;*/
   top: 10px;
   left: 10px;
-  z-index: 10; /* 更高的层级 */
+  z-index: 10;
   background-color: rgba(40, 40, 40, 0.7);
 }
 #cesiumContainer {
@@ -282,25 +471,21 @@ export default {
   padding: 0;
   overflow: hidden;
 }
-
 .eqListfade-enter-active, .eqListfade-leave-active {
   transition: opacity .5s;
 }
-
 .eqListfade-enter, .eqListfade-leave-to {
   opacity: 0;
 }
-
 .button-container {
   position: absolute;
   padding: 15px;
   border-radius: 5px;
   top: 10px;
   left: 10px;
-  z-index: 10; /* 更高的层级 */
+  z-index: 10;
   background-color: rgba(40, 40, 40, 0.7);
 }
-
 .latlon-legend {
   pointer-events: auto;
   position: absolute;
@@ -312,7 +497,6 @@ export default {
   width: 125px;
   box-sizing: content-box;
 }
-
 .modelAdj {
   color: white;
   margin-bottom: 15px;
